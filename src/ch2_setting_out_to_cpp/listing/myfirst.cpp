0811ee19087dbcd070af5b003336f90005e15418
yang <ponny213@hotmail.com>
2020-01-04 01:02:05 -0600

    fisrt commit

---
+++ src/ch2_setting_out_to_cpp/listing/myfirst.cpp
@@ -0,0 +1,10 @@
+// List 2.1 myfirst.cpp
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come on and C++ me some time!" << endl;
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 1;
+}
