250eaa9b5dbc3a64af3de326a9274024d72ee4d3
Yang Yang <owenyuon@gmail.com>
2021-06-09 17:54:38 -0500

    ch10: first two listings

---
+++ src/ch10_objects_and_classes/listing/usestok1.cpp
@@ -0,0 +1,35 @@
+// usestok1.cpp -- using the Stock class
+
+#include <iostream>
+#include "stock10.h"
+
+int main()
+{
+	{
+		using std::cout;
+		cout << "Using constructors to create new objects. \n";
+		Stock stock1("NanoSmart", 12, 20.0);
+		stock1.show();
+
+		Stock stock2("Boffo Objects", 2, 2.0);
+		stock2.show();
+
+		cout << "Assigning stock1 to stock2:\n";
+		stock2 = stock1;
+		cout << "Listing stock1 and stock2:\n";
+		stock1.show();
+		stock2.show();
+
+		cout << "Using a constructor to reset a object\n";
+		stock1 = Stock("Nifty Foods", 10, 50.0);  // temp object
+		cout << "Revised stock:\n";
+		stock1.show();
+		
+		cout << "Done\n";
+
+		stock1.update(1000.0);
+		stock1.show();
+	}
+
+	return 0;
+}
